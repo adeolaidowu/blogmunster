66f8f2f79742bfea3724d73d8792bea505b5bd10
adeola idowu <adeolaidowu27@gmail.com>
2020-04-14 16:32:44 +0100

    setup production server for deployment

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+const publicPath = path.join(__dirname, "build");
+
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(publicPath, "index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
